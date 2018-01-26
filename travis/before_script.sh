262f662312f8cd7a5baf3a0f56c65bf80749cd16
Kerry Billingham <contact@AvionicEngineers.com>
2018-01-26 23:27:51 +0000

    Build command conditional upon branch.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#if [ "${TRAVIS_BRANCH}" = 'dev' ]; then
+if [ "${TRAVIS_BRANCH}" = 'dev' ] || [ "${TRAVIS_BRANCH}" = 'master' ]; then
     mkdir ~/.m2
     cp ${TRAVIS_BUILD_DIR}/travis/settings-security.xml ~/.m2/
 
@@ -9,7 +9,6 @@
     openssl aes-256-cbc -K $encrypted_431ce540c668_key -iv $encrypted_431ce540c668_iv -in ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml.enc -out ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml -d
 
 
-    #gpg --fast-import --no-tty ${TRAVIS_BUILD_DIR}/travis/codesigning.asc &> /dev/null
-    gpg --fast-import ${TRAVIS_BUILD_DIR}/travis/signing.asc
+    gpg --fast-import --no-tty ${TRAVIS_BUILD_DIR}/travis/codesigning.asc &> /dev/null
 
-#fi
+fi
