c4b76fdf5a03ec3b7b9a76087ee658fc04a66623
Kerry Billingham <contact@AvionicEngineers.com>
2018-01-27 18:50:31 +0000

    Correcting build script on dev.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -31,9 +31,10 @@ if [ "$TRAVIS_BRANCH" == "dev" ] && [ ${IS_RELEASE} -eq ${FALSE} ]; then
     mkdir -p ~/.m2
     cp ${TRAVIS_BUILD_DIR}/travis/settings-security.xml ~/.m2/
 
-    openssl aes-256-cbc -K $encrypted_4f3061b44f4c_key -iv $encrypted_4f3061b44f4c_iv -in ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml.enc -out ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml -d
+    openssl aes-256-cbc -K $encrypted_259161014934_key -iv $encrypted_259161014934_iv -in ${TRAVIS_BUILD_DIR}/travis/signing.asc.enc -out ${TRAVIS_BUILD_DIR}/travis/signing.asc -d
 
-    openssl aes-256-cbc -K $encrypted_6546a769d586_key -iv $encrypted_6546a769d586_iv -in ${TRAVIS_BUILD_DIR}/travis/signing.asc.enc -out ${TRAVIS_BUILD_DIR}/travis/signing.asc -d
+    openssl aes-256-cbc -K $encrypted_431ce540c668_key -iv $encrypted_431ce540c668_iv -in ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml.enc -out ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml -d
+
     gpg --fast-import --no-tty ${TRAVIS_BUILD_DIR}/travis/signing.asc &> /dev/null
 fi
 
@@ -42,9 +43,10 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ ${IS_RELEASE} -eq ${TRUE} ]; then
     mkdir -p ~/.m2
     cp ${TRAVIS_BUILD_DIR}/travis/settings-security.xml ~/.m2/
 
-    openssl aes-256-cbc -K $encrypted_4f3061b44f4c_key -iv $encrypted_4f3061b44f4c_iv -in ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml.enc -out ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml -d
+    openssl aes-256-cbc -K $encrypted_259161014934_key -iv $encrypted_259161014934_iv -in ${TRAVIS_BUILD_DIR}/travis/signing.asc.enc -out ${TRAVIS_BUILD_DIR}/travis/signing.asc -d
 
-    openssl aes-256-cbc -K $encrypted_6546a769d586_key -iv $encrypted_6546a769d586_iv -in ${TRAVIS_BUILD_DIR}/travis/signing.asc.enc -out ${TRAVIS_BUILD_DIR}/travis/signing.asc -d
+    openssl aes-256-cbc -K $encrypted_431ce540c668_key -iv $encrypted_431ce540c668_iv -in ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml.enc -out ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml -d
+
     gpg --fast-import --no-tty ${TRAVIS_BUILD_DIR}/travis/signing.asc &> /dev/null
 fi
 
